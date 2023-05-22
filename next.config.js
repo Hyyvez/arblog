d3231a0d11b9cadc45ed3bdaa9494bdc74e9e9b6
Hyyvez <90178724+Hyyvez@users.noreply.github.com>
2023-05-22 12:40:43 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,5 @@
+// This configuration file is used to set the correct base path for the Step Page.
+// The base path ensures that resources, such as images, from the 'public' folder are loaded correctly.
+module.exports = {
+  basePath: '/arblog', //  place with your desired base path
+};
